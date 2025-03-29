bf9bda6fba25837f68d4d97f0e912199f29ab4b9
Jonathan-Agreda <flavioagreda1990@gmail.com>
2025-03-28 21:41:49 -0500

    server basic web

---
+++ index.js
@@ -0,0 +1,61 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    req.url === "/"
+      ? "index.html"
+      : req.url === "/about"
+      ? "about.html"
+      : req.url === "/contact-me"
+      ? "contact-me.html"
+      : "404.html"
+  );
+
+  // Set the content type
+  const extname = path.extname(filePath);
+  let contentType = "text/html";
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  // Read and serve the file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        // File not found, serve 404
+        fs.readFile(path.join(__dirname, "404.html"), (err, content) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(content, "utf8");
+        });
+      } else {
+        // Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
